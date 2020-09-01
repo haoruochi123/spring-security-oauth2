608f089bd2e766fba9d3b07ef02cdd6f8eb1dca0
郝 若池 <haorc@travelsky.com>
2020-09-01 17:33:38 +0800

    init

---
+++ oauth2-resource/src/test/java/com/hrc/oauth2/Oauth2ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrc.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
